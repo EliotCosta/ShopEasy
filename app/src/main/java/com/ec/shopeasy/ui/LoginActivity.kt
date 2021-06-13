0eb3c32e1b58d7d3d85b89c3fb7dd39e548baf5e
eliot <costa.eliot@gmail.com>
2021-06-14 00:05:46 +0200

    premières pages

--- app/src/main/java/com/ec/shopeasy/ui/LoginActivity.kt
+++ app/src/main/java/com/ec/shopeasy/ui/LoginActivity.kt
@@ -1,12 +1,60 @@
 package com.ec.shopeasy.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import com.ec.shopeasy.R
+import com.squareup.picasso.Picasso
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLogin2 = findViewById<Button>(R.id.btnLogin2)
+        btnLogin2.setOnClickListener(this)
+        val btnShow = findViewById<Button>(R.id.btnShow)
+        btnShow.setOnClickListener(this)
+        val image = findViewById<ImageView>(R.id.imageView)
+        val pseudo = findViewById<EditText>(R.id.pseudo)
+        val mdp = findViewById<EditText>(R.id.mdp)
+
+        Picasso.get()
+            .load("https://www.wedemain.fr/wp-content/uploads/2020/04/17599089-22052598.jpg")
+            .into(image)
+
+
+
+        btnShow.setOnClickListener {
+            if (btnShow.text.toString().equals("Show")) {
+                mdp.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                btnShow.text = "Hide"
+            } else {
+                mdp.transformationMethod = PasswordTransformationMethod.getInstance()
+                btnShow.text = "Show"
+            }
+        }
+
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnLogin2 -> {
+
+                // à faire : vérification des identifiants de l'utilisateur
+
+                // vers LocalisationActivity
+
+                // Intent explicite
+                var versLocalisationActivity: Intent = Intent(this@LoginActivity, LocalisationActivity::class.java)
+                startActivity(versLocalisationActivity)
+            }
+        }
     }
 }
--- app/src/main/java/com/ec/shopeasy/ui/SignupActivity.kt
+++ app/src/main/java/com/ec/shopeasy/ui/SignupActivity.kt
@@ -1,12 +1,32 @@
 package com.ec.shopeasy.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.ec.shopeasy.R
 
-class SignupActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.button -> {
+
+                // vers LocalisationActivity
+
+                // Intent explicite
+                var versLoginActivity: Intent =
+                    Intent(this@SignupActivity, LoginActivity::class.java)
+                startActivity(versLoginActivity)
+            }
+        }
     }
 }
